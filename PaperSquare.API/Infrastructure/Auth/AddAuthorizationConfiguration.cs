7410eae0753cb921a476d01a27e0b47c025d77a1
Dino Behrem <dinobehrem23@gmail.com>
2022-10-31 19:33:37 +0100

    Authentication And identity configuration!

---
+++ PaperSquare.API/Infrastructure/Auth/AddAuthorizationConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace PaperSquare.API.Infrastructure.Auth
+{
+    public static class AddAuthorizationConfiguration
+    {
+        public static IServiceCollection AddAuthorizationConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
--- PaperSquare.API/Program.cs
+++ PaperSquare.API/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using PaperSquare.API.Infrastructure.Auth;
+using PaperSquare.API.Infrastructure.SwaggerGen;
 using PaperSquare.API.Infrastructure.Versioning;
 using PaperSquare.Data.Data;
 using Serilog;
@@ -23,8 +25,10 @@ builder.Services.AddDbContext<PaperSquareDbContext>(options => options.UseSqlSer
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.SwaggerGenConfig();
 
+builder.Services.AddIdentityConfig(builder.Configuration);
+builder.Services.AddAuthenticationConfig(builder.Configuration);
 builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
 
 var app = builder.Build();

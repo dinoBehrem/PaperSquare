cfd2ebd67b6e729c32e34fa3790d0471d37a3a2d
Dino Behrem <dinobehrem23@gmail.com>
2022-11-19 19:41:11 +0100

    Current user implementation!

--- PaperSquare.API/Feature/Users/V_1/UsersController.cs
+++ PaperSquare.API/Feature/Users/V_1/UsersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PaperSquare.API.Infrastructure.Versioning;
+using PaperSquare.Core.Infrastructure.CurrentUserAccessor;
 using PaperSquare.Core.Models.Identity;
 using PaperSquare.Core.Permissions;
 using PaperSquare.Infrastructure.Features.UserManagement;
@@ -15,36 +16,17 @@ namespace PaperSquare.API.Feature.Users.V_1
     public class UsersController : ControllerBase
     {
         private readonly IUserService _userService;
-        private readonly RoleManager<Role> userManager;
+        private readonly ICurrentUser _currentUser;
 
-        public UsersController(IUserService userService, RoleManager<Role> userManager)
+        public UsersController(
+            IUserService userService, 
+            ICurrentUser currentUser)
         {
             _userService = userService;
-            this.userManager = userManager;
+            _currentUser = currentUser;
         }
 
         #region GET
-        #endregion GET
-
-        #region POST
-
-        [HttpPost("user-registration")]
-        [MapToApiVersion(ApiVersions.V_1)]
-        [Produces(MediaTypeNames.Application.Json)]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [AllowAnonymous]
-        public async Task<IActionResult> Registration([FromBody]UserRegistrationDto registrationRequest)
-        {
-            var result = await _userService.CreateUserAsync(registrationRequest);
-
-            if (!result.IsSuccess)
-            {
-                return BadRequest(new { errors = result.Errors, validationErrors = result.ValidationErrors });
-            }
-
-            return CreatedAtAction(nameof(Registration), result);
-        }
 
         [HttpGet("get-users")]
         [MapToApiVersion(ApiVersions.V_1)]
@@ -65,16 +47,35 @@ namespace PaperSquare.API.Feature.Users.V_1
         }
 
 
-        [HttpPost("add-role")]
-        [AllowAnonymous]
-        public async Task<IActionResult> AddRole()
+        [HttpGet("get-user-info")]
+        [Authorize]
+        public async Task<IActionResult> GetUserInfo()
         {
-            await userManager.CreateAsync(new Role()
-            {
-                Name = Roles.RegisteredUser
-            });
+           var userInfo = new { id = _currentUser.Id, username = _currentUser.UserName, email = _currentUser.Email, roles = _currentUser.Roles };
 
-            return Ok();
+            return Ok(userInfo);
+        }
+
+        #endregion GET
+
+        #region POST
+
+        [HttpPost("user-registration")]
+        [MapToApiVersion(ApiVersions.V_1)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [AllowAnonymous]
+        public async Task<IActionResult> Registration([FromBody]UserRegistrationDto registrationRequest)
+        {
+            var result = await _userService.CreateUserAsync(registrationRequest);
+
+            if (!result.IsSuccess)
+            {
+                return BadRequest(new { errors = result.Errors, validationErrors = result.ValidationErrors });
+            }
+
+            return CreatedAtAction(nameof(Registration), result);
         }
 
         #endregion POST
